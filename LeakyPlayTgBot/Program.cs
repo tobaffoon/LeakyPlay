db349f1d688118c0f7ef8575c798de7c2c414d1c
ALEXEI MAZIAR <a.mathjar@gmail.com>
2024-11-14 22:51:29 +0700

    Move tg bot to separate class

--- LeakyPlayTgBot/Program.cs
+++ LeakyPlayTgBot/Program.cs
@@ -1,9 +1,5 @@
-﻿using Telegram.Bot;
-using Telegram.Bot.Polling;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.ReplyMarkups;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.UserSecrets;
+﻿using Microsoft.Extensions.Configuration;
+using LeakyPlayTgBot.TelegramBot;
 
 namespace LeakyPlayTelegramBot
 {
@@ -21,67 +17,11 @@ namespace LeakyPlayTelegramBot
          {
             throw new ArgumentNullException($"{ConfigTokenKey} is not defined correctly");
          }
+
          var cts = new CancellationTokenSource();
-         var client = new TelegramBotClient(botToken);
-
-         ReceiverOptions receiverOptions = new()
-         {
-            AllowedUpdates = []
-         };
-         client.StartReceiving(Update, Error, receiverOptions: receiverOptions, cancellationToken: cts.Token);
+         var leakyBot = new LeakyPlayBot(botToken, cts.Token);
+         leakyBot.StartBot();
          Console.ReadLine();
-      }
-
-      private static async Task Update(ITelegramBotClient client, Update update, CancellationToken token)
-      {
-         System.Console.WriteLine("New req");
-         if (update.Message == null)
-         {
-            //aa
-            return;
-         }
-
-         var message = update.Message;
-         if (message.Text == null)
-         {
-            //b
-            System.Console.WriteLine("No Text");
-         }
-         else if (message.Text.ToLower() == "help")
-         {
-            await client.SendTextMessageAsync(message.Chat.Id, "Nuh uh");
-         }
-         else
-         {
-            await client.SendTextMessageAsync(message.Chat.Id, "I don't understant", replyMarkup: GetButtons());
-         }
-
-         if (message.Photo == null)
-         {
-            // c
-            System.Console.WriteLine("No Photo");
-         }
-         else
-         {
-
-            await client.SendTextMessageAsync(message.Chat.Id, $"I Don't like this photo, dear {message.Chat.FirstName}");
-         }
-
-
-         return;
-      }
-
-      private static async Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
-      {
-         throw new NotImplementedException();
-      }
-
-      private static IReplyMarkup GetButtons()
-      {
-         return new InlineKeyboardMarkup([
-             InlineKeyboardButton.WithCallbackData("Info", "Help"),
-                InlineKeyboardButton.WithCallbackData("Enter a room", "hint1")
-         ]);
       }
    }
 }
